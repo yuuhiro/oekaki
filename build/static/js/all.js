6fd9df15c07e9399c2a0825da4a395a9c02fdc76
yuuhiro <yuuhiro@yuuAir.local>
2014-05-24 03:21:20 +0900

    お絵描きの基本

--- build/static/js/all.js
+++ build/static/js/all.js
@@ -17584,3 +17584,53 @@ return jQuery;
 
 }));
 
+var OekakiView = Backbone.View.extend({
+	el: "#oekaki",
+	initialize: function () {
+
+		this.canvas = this.$el.find("#oekakiCanvas")[0];
+		this.ctx = this.canvas.getContext('2d');
+		
+		this.offsetX = this.canvas.offsetLeft;
+		this.offsetY = this.canvas.offsetTop;
+		
+		this.drawFlag = false;
+		this.strokeStyle = 'black';
+		this.lineWidth = 3;
+	},
+	events: {
+		'mousedown #oekakiCanvas': 'drawStart',
+		'mousemove #oekakiCanvas': 'draw',
+		'mouseup #oekakiCanvas': 'drawEnd'
+	},
+	drawStart: function(e) {
+		this.drawFlag = true;
+		
+		this.lastX = e.pageX - this.offsetX;
+		this.lastY = e.pageY - this.offsetY;
+	},
+	drawEnd: function() {
+		this.drawFlag = false;
+	},
+	draw: function(e) {
+		if (!this.drawFlag) return;
+
+		var x = e.pageX - this.offsetX;
+		var y = e.pageY - this.offsetY;
+		
+		this.ctx.strokeStyle = this.strokeStyle;
+		this.ctx.lineWidth = this.lineWidth;
+
+		this.ctx.beginPath();
+		this.ctx.moveTo(this.lastX, this.lastY);
+		this.ctx.lineTo(x, y);
+		this.ctx.lineCap = "round";
+		this.ctx.stroke();
+		this.ctx.closePath();
+		this.ctx.lineJoin = 'round';
+		
+		this.lastX = x;
+		this.lastY = y;
+	}
+});
+new OekakiView();
