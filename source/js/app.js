281b84dd6479f22df1be9f717ce516390dc800d9
yuuhiro <yuuhiro@yuuAir.local>
2014-06-06 02:09:28 +0900

    制限チェック

--- source/js/app.js
+++ source/js/app.js
@@ -16,14 +16,33 @@ var OekakiView = Backbone.View.extend({
 		this.strokeStyle = 'black';
 		this.lineWidth = 2;
 
+		var defaultWidth = 100;
+		var defaultHeight = defaultWidth/this.ratio;
 		this.square = {
-			width: "",
-			height: "",
-			corners:[]
+			width: defaultWidth,
+			height: defaultHeight,
+			corners:[
+				{
+					x: 50,
+					y: 50
+				},
+				{
+					x: 50 + defaultWidth,
+					y: 50
+				},
+				{
+					x: 50 + defaultWidth,
+					y: 50 + defaultHeight
+				},
+				{
+					x: 50,
+					y: 50 + defaultHeight
+				}
+			]
 		};
 
 		// 矩形を描画
-		this.render(50, 50, 100*this.ratio, 100);
+		this.render();
 	},
 	events: {
 		'mousedown #oekakiCanvas': 'mouseDownHandler',
@@ -79,21 +98,16 @@ var OekakiView = Backbone.View.extend({
 	},
 	mouseUpHandler: function() {
 		console.log(this.square);
+		console.log(this.drawMode);
 		this.drawFlag = false;
 	},
 	mouseMoveHandler: function(e) {
 		if (!this.drawFlag) return;
 
-		var width, // 矩形の幅
-			height, // 矩形の高さ
-			currentX, // マウス移動時のx座標
+		var currentX, // マウス移動時のx座標
 			currentY, // マウス移動時のy座標
 			distanceX, // クリック時のマウスx座標からのx軸移動距離
-			distanceY, // クリック時のマウスx座標からのy軸移動距離
-			basePointX, // 矩形の基点となるx座標（矩形の左上）
-			basePointY, // 矩形の基点となるy座標（矩形の左上）
-			endPointX, // 矩形の終点となるx座標（矩形の右下）
-			endPointY; // 矩形の終点となるy座標（矩形の右下）
+			distanceY; // クリック時のマウスx座標からのy軸移動距離
 
 		// マウス移動時のx,y座標を取得
 		currentX = e.pageX - this.offsetX;
@@ -107,82 +121,131 @@ var OekakiView = Backbone.View.extend({
 		this.startX = currentX;
 		this.startY = currentY;
 
+		if(this.drawMode != "move")
+		{
+			if(Math.abs(distanceX) > Math.abs(distanceY))
+			{
+				distanceY = distanceX/this.ratio;
+				if(this.drawMode === 2 || this.drawMode === 3)
+				{
+					distanceY = -distanceY;
+				}
+			}
+			else
+			{
+				distanceX = distanceY*this.ratio;
+				if(this.drawMode === 2 || this.drawMode === 3)
+				{
+					distanceX = -distanceX;
+				}
+			}
+		}
+
+		// 矩形の座標を更新
+		this.updateCorners(distanceX, distanceY);
+
+		// 矩形を描画
+		this.render();
+	},
+	updateCorners: function(distanceX, distanceY) {
+		// ローカル変数にクローンしておく
+		var _corners = _.clone(this.square.corners, true);
 		if(this.drawMode === "move")
 		{
-			// 矩形の移動時は、移動前の矩形の基点x,y座標に移動距離をプラスして
+			// 矩形の移動時は、移動前の矩形の全ての四隅のx,y座標に移動距離をプラスして
 			// 新たな矩形の基点座標を導きだす
-			basePointX = this.square.corners[0].x + distanceX;
-			basePointY = this.square.corners[0].y + distanceY;
-
-			// 移動時は矩形のwidthとheightは変わらない
-			width = this.square.width;
-			height = this.square.height;
+			_.each(_corners, function(item, i) {
+				item.x += distanceX;
+				item.y += distanceY;
+			});
 		}
 		else
 		{
 			switch(this.drawMode){
 			case 1:
-				basePointX = this.square.corners[0].x + distanceX;
-				basePointY = this.square.corners[0].y + distanceY;
+				_corners[0].x += distanceX;
+				_corners[3].x += distanceX;
+				_corners[0].y += distanceY;
+				_corners[1].y += distanceY;
 			break;
 			case 2:
-				basePointX = this.square.corners[3].x;
-				basePointY = this.square.corners[3].y;
+				_corners[1].x += distanceX;
+				_corners[2].x += distanceX;
+				_corners[1].y += distanceY;
+				_corners[0].y += distanceY;
 			break;
 			case 3:
-				basePointX = this.square.corners[1].x;
-				basePointY = this.square.corners[1].y;
+				_corners[3].x += distanceX;
+				_corners[0].x += distanceX;
+				_corners[3].y += distanceY;
+				_corners[2].y += distanceY;
 			break;
 			case 4:
-				basePointX = this.square.corners[0].x;
-				basePointY = this.square.corners[0].y;
+				_corners[2].x += distanceX;
+				_corners[1].x += distanceX;
+				_corners[2].y += distanceY;
+				_corners[3].y += distanceY;
 			break;
 			}
-			width = this.square.width + Math.abs(distanceX);
-			height = this.square.height + Math.abs(distanceY);
 		}
 
-		// 矩形を描画
-		this.render(basePointX, basePointY, width, height);
+		if(!this.checkLimit(_corners)) return;
+		// 制限内であれば値を更新する
+		this.square.corners = $.extend(this.square.corners, _corners);
 	},
-	render: function(x, y, width, height) {
+	checkLimit: function(_corners) {
+		var basePointX = _corners[0].x,
+			basePointY = _corners[0].y,
+			endPointX  = _corners[2].x,
+			endPointY  = _corners[2].y,
+			width      = endPointX - basePointX,
+			height     = endPointY - basePointY;
+
+		// 基点と終点がキャンバス内をはみ出さないように
+		if((this.drawMode === 1 || this.drawMode === 3) && (basePointX <= 0 || basePointY <= 0)) return false;
+		if((this.drawMode === 2 || this.drawMode === 4) && (endPointX  >= this.canvas.width || endPointY  >= this.canvas.height)) return false;
+		if(this.drawMode === "move")
+		{
+			if(basePointX <= 0
+			|| basePointY <= 0
+			|| endPointX  >= this.canvas.width 
+			|| endPointY  >= this.canvas.height
+			)
+			{
+				return false;
+			}
+		}
+		else
+		{
+			// minimumチェック
+			if(width <= 50 && height <= 50/this.ratio) return false;
+			
+		}
+		return true;
+	},
+	render: function() {
+		var basePointX = this.square.corners[0].x;
+		var basePointY = this.square.corners[0].y;
+		var endPointX  = this.square.corners[2].x;
+		var endPointY  = this.square.corners[2].y;
+		var width      = endPointX - basePointX;
+		var height     = endPointY - basePointY;
+
 		this.ctx.strokeStyle = this.strokeStyle;
 		this.ctx.lineWidth = this.lineWidth;
 
 		// 以前の矩形が残っているので、一度canvasをクリアする
 		this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
-		
-		this.ctx.strokeRect(x, y, width, height);
+		this.ctx.strokeRect(basePointX, basePointY, width, height);
 
 		// 矩形の幅、高さと四隅の座標を格納
-		this.square = {
+		var square = {
 			width: width,
 			height: height,
-			cx: x + width/2,
-			cy: y + height/2,
-			corners: [
-				// 左上（基点）
-				{
-					x: x,
-					y: y
-				},
-				// 右上
-				{
-					x: x + width,
-					y: y
-				},
-				// 右下（終点）
-				{
-					x: x + width,
-					y: y + height
-				},
-				// 左下
-				{
-					x: x,
-					y: y + height
-				}
-			]
-		}
+			cx: basePointX + width/2,
+			cy: basePointY + height/2
+		};
+		this.square = $.extend(this.square, square);
 	}
 });
 new OekakiView();
